4039d5d2faf395da0722b4fb542896dc77750ef1
ayazhafiz <ayaz.hafiz.1@gmail.com>
2021-06-18 15:01:56 -0500

    Simple type inference

--- dnf-plus/ft.ml
+++ dnf-plus/ft.ml
@@ -1,11 +1,10 @@
+open Language
 open Language.Ast
 open Typecheck
 
 let string_of_ty = Language.string_of_ty
 
-let string_of_term =
-  Language.string_of_term (fun ty ->
-      !ty |> Option.map dnf_plus |> Option.map string_of_ty)
+let string_of_term = Language.string_of_term dnf_plus
 
 let rec flatten_ty_in_term t =
   match t with
@@ -13,14 +12,12 @@ let rec flatten_ty_in_term t =
   | Tup (ts, ty) -> Tup (List.map flatten_ty_in_term ts, ty)
   | App (fn, t, ty) -> App (fn, List.map flatten_ty_in_term t, ty)
   | Dec (fn, params, body, cont, ty) ->
-      Dec
-        ( fn,
-          List.map
-            (fun { name; ty } -> { name; ty = Option.map flatten_ty ty })
-            params,
-          flatten_ty_in_term body,
-          flatten_ty_in_term cont,
-          ty )
+      List.iter
+        (function
+          | _, ty, _ when needs_inference !ty -> ()
+          | _, ty, _ -> ty := flatten_ty !ty)
+        params;
+      Dec (fn, params, flatten_ty_in_term body, flatten_ty_in_term cont, ty)
   | If (var, isty, then', else', ty) ->
       If
         ( var,
@@ -47,3 +44,5 @@ let dnf_plus = Typecheck.dnf_plus
 let ( <: ) = Typecheck.( <: )
 
 let typecheck = Typecheck.typecheck
+
+let infer_types = Infer.infer_types
