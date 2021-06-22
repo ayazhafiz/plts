d5205c755873a23a3d89a516e47c472ea87f8153
CircleCI <builds@circleci.com>
2021-06-22 04:49:56 +0100

    CircleCI build 33

--- dnf-plus/www/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Run from repo root
-dune build dnf-plus/ft.js
-cp dnf-plus/www/index-template.html dnf-plus/www/index.html
-sed -i"" "s/CACHE_BUST/$(git rev-parse HEAD)/g" dnf-plus/www/index.html
