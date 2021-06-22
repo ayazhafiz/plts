2e76f63025185c37da5f509095c469e283abaecf
ayazhafiz <ayaz.hafiz.1@gmail.com>
2021-06-21 22:11:22 -0500

    Fixup site publishing

--- dnf-plus/www/build.sh
+++ dnf-plus/www/build.sh
@@ -2,4 +2,4 @@
 # Run from repo root
 dune build dnf-plus/ft.js
 cp dnf-plus/www/index-template.html dnf-plus/www/index.html
-sed -i "" "s/CACHE_BUST/$(git rev-parse HEAD)/g" dnf-plus/www/index.html
+sed -i"" "s/CACHE_BUST/$(git rev-parse HEAD)/g" dnf-plus/www/index.html
