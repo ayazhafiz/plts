f7b5ad3a2cb62d8bb97622db420d6f98b3d890a5
ayazhafiz <ayaz.hafiz.1@gmail.com>
2021-08-14 13:25:32 -0400

    Info on codegen

--- TAL/www/build.sh
+++ TAL/www/build.sh
@@ -2,4 +2,4 @@
 # Run from repo root
 dune build TAL/tal.js
 cp TAL/www/index-template.html TAL/www/index.html
-sed -i"" "s/CACHE_BUST/$(git rev-parse HEAD)/g" TAL/www/index.html
+sed -i "" "s/CACHE_BUST/$(git rev-parse HEAD)/g" TAL/www/index.html
