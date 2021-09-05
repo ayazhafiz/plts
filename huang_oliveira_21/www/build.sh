6aeb3c9dbc334d7c2b27a230f8aceaecf0ca542e
ayazhafiz <ayaz.hafiz.1@gmail.com>
2021-09-04 22:21:34 -0400

    Fix unix sed build issue

--- huang_oliveira_21/www/build.sh
+++ huang_oliveira_21/www/build.sh
@@ -2,4 +2,4 @@
 # Run from repo root
 dune build huang_oliveira_21/ho21.js
 cp huang_oliveira_21/www/index-template.html huang_oliveira_21/www/index.html
-sed -i "" "s/CACHE_BUST/$(git rev-parse HEAD)/g" huang_oliveira_21/www/index.html
+sed -i"" "s/CACHE_BUST/$(git rev-parse HEAD)/g" huang_oliveira_21/www/index.html
