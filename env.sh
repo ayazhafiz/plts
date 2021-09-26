f739f296950a95e62003e2961c62e029095461cc
ayazhafiz <ayaz.hafiz.1@gmail.com>
2021-09-26 13:21:41 -0400

    Wrap dune in node_modules switcher iff node_modules exists

--- env.sh
+++ env.sh
@@ -3,7 +3,7 @@
 # Wrapper around dune to avoid trying to examine node_modules when running dune
 # from root.
 dune() {
-  if [ "$(git rev-parse --show-toplevel 2>/dev/null)" == "$(pwd)" ]; then
+  if [ "$(git rev-parse --show-toplevel 2>/dev/null)" == "$(pwd)" ] && [ -d "node_modules" ]; then
     mv node_modules _nm; command dune "$@"; mv _nm node_modules
   else
     command dune "$@"
