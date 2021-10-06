093c3ea74a4a4749d4ddee6bb618b402d8747987
ayazhafiz <ayaz.hafiz.1@gmail.com>
2021-10-05 21:26:53 -0400

    Check TS code during builds

--- www/gatsby-config.js
+++ www/gatsby-config.js
@@ -9,12 +9,15 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-plugin-typescript`,
+      resolve: 'gatsby-plugin-typescript',
       options: {
-        isTSX: true,          // defaults to false
-        jsxPragma: `jsx`,     // defaults to "React"
-        allExtensions: true,  // defaults to false
+        isTSX: true,
+        jsxPragma: `jsx`,
+        allExtensions: true,
       },
+    },
+    {
+      resolve: 'gatsby-plugin-typescript-checker',
     },
   ],
 };
