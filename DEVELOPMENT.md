086de4196661a1ff779162bd8d9bb07afede3740
Ayaz Hafiz <ayaz@trytreater.com>
2025-05-11 19:53:14 -0400

    gash

---
+++ DEVELOPMENT.md
@@ -0,0 +1,3 @@
+## Opam
+
+Pin ocaml_of_js to '5.8.1', at least until https://github.com/webpack/webpack/issues/14166 is resolved.
--- www/gatsby-node.js
+++ www/gatsby-node.js
@@ -1,6 +1,4 @@
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
-const webpack = require('webpack');
-const path = require('path');
 
 exports.onCreateWebpackConfig = ({
   stage,
@@ -10,16 +8,11 @@ exports.onCreateWebpackConfig = ({
   actions,
 }) => {
   actions.setWebpackConfig({
+    module: {
+      rules: [],
+    },
     plugins: [
       new MonacoWebpackPlugin({languages: ['c', 'typescript', 'javascript']}),
-      // Simple solution: Replace all node: imports with empty objects
-      new webpack.NormalModuleReplacementPlugin(
-        /^node:/,
-        (resource) => {
-          // Replace with an empty module
-          resource.request = path.resolve(__dirname, 'src/utils/empty-module.js');
-        }
-      ),
     ],
     resolve: {
       fallback: {
@@ -27,8 +20,10 @@ exports.onCreateWebpackConfig = ({
         fs: false,
         module: false,
         pnpapi: false,
-        node: false
       },
     },
+    node: {
+      fs: 'empty',
+    }
   })
 }
